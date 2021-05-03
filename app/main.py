6b2071bce002c3e925422e90e0550bfb2ec09bcc
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-03 11:25:10 +0300

    Create GraphAnalysis

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def main():
     vk_session = VK(login, password)
 
     start = time.time()
-    G = vk_session.get_graph_of_users_and_his_friends(444597545, 2).nodes()
+    G = vk_session.get_graph_of_users_and_his_friends(444597545).nodes()
     end = time.time()
     start_pool = time.time()
-    G_pool = vk_session.get_graph_of_users_and_his_friends_pool(444597545, 2).nodes()
+    G_pool = vk_session.get_graph_of_users_and_his_friends_pool(444597545).nodes()
     end_pool = time.time()
     print(G == G_pool)
     print(end - start, end_pool - start_pool)
